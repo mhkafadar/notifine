30df4237e999d96a12f63610fd3afb4434079304
Mehmet Hüseyin Kafadar <43652059+mhkafadar@users.noreply.github.com>
2023-03-04 02:07:21 +0300

    feat: new branch creation notifications (#10)

--- src/webhooks/gitlab/webhook_handlers/new_branch_push.rs
+++ src/webhooks/gitlab/webhook_handlers/new_branch_push.rs
@@ -1,12 +1,6 @@
 use crate::webhooks::gitlab::http_server::GitlabEvent;
-use crate::webhooks::gitlab::webhook_handlers::new_branch_push::new_branch_push;
 
-pub fn handle_push_event(gitlab_event: &GitlabEvent) -> String {
-    // check if it is a new branch push
-    if gitlab_event.before.as_ref().unwrap() == "0000000000000000000000000000000000000000" {
-        return new_branch_push(&gitlab_event);
-    }
-
+pub fn new_branch_push(gitlab_event: &GitlabEvent) -> String {
     let branch_ref = &gitlab_event.r#ref.as_ref().unwrap();
     let branch_name = branch_ref.split('/').last().unwrap();
     let project = &gitlab_event.project;
@@ -16,10 +10,17 @@ pub fn handle_push_event(gitlab_event: &GitlabEvent) -> String {
     // set project_url to project.homepage if not none set "dummy_url" otherwise
     let fallback_project_url = "empty_gitlab_project_url".to_owned();
     let project_url = &project.homepage.as_ref().unwrap_or(&fallback_project_url);
+    let user = gitlab_event.user_name.as_ref().unwrap();
 
-    let mut commit_paragraph = String::new();
+    let mut commit_paragraph = format!(
+        "<b>{user}</b> \
+        pushed a new branch <a href=\"{project_url}/-/tree/\
+        {branch_name}\">{branch_name}</a> to <a href=\"{project_url}\">\
+        {project_name}</a>\n\n",
+    );
 
-    for commit in gitlab_event.commits.as_ref().unwrap() {
+    // loop through 4 commits from end of commits vector
+    for commit in gitlab_event.commits.as_ref().unwrap().iter().rev().take(4) {
         log::info!("Commit: {}", commit.message);
         log::info!("Commit url: {}", commit.url);
         log::info!("Commit author: {}", commit.author.name);
@@ -28,10 +29,10 @@ pub fn handle_push_event(gitlab_event: &GitlabEvent) -> String {
         let commit_message = &commit.message.trim_end();
         let commit_author_name = &commit.author.name;
 
-        // commit_paragraph.push_str(&format!("{}: [{}]({}) to [{}:{}]({})\n", commit_author_name, commit_message, commit_url, project_name, branch_name, project_url));
         commit_paragraph.push_str(&format!(
-            "<b>{}</b>: <a href=\"{}\">{}</a> to <a href=\"{}\">{}:{}</a>\n",
-            commit_author_name, commit_url, commit_message, project_url, project_name, branch_name
+            "<b>{commit_author_name}</b>: \
+            <a href=\"{commit_url}\">{commit_message}</a> \
+            to <a href=\"{project_url}\">{project_name}:{branch_name}</a>\n",
         ));
     }
 
