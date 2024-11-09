7082a1c2394cfec29fbe6290ff5694ec60459068
sekomer <a.serkanaksoz@gmail.com>
2024-11-09 12:37:32 +0300

    feat: add issue description functionality for gitlab

--- src/webhooks/gitlab/webhook_handlers/issue.rs
+++ src/webhooks/gitlab/webhook_handlers/issue.rs
@@ -1,6 +1,16 @@
 use actix_web::web;
+use html_escape::encode_text;
 use serde::Deserialize;
 use ureq::serde_json;
+
+#[derive(Debug)]
+enum IssueAction {
+    Open,
+    Update,
+    Close,
+    Reopen,
+    Other(String),
+}
 
 #[derive(Debug, Deserialize)]
 struct IssueEvent {
@@ -13,6 +23,7 @@ struct IssueDetails {
     title: String,
     url: String,
     action: String,
+    description: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -28,16 +39,31 @@ pub fn handle_issue_event(body: &web::Bytes) -> String {
     let title = &issue_details.title;
     let action = &issue_details.action;
     let user_name = &issue_event.user.name;
+    let description = encode_text(&issue_details.description);
 
-    if action == "open" {
-        format!("<b>{user_name}</b> opened a new issue <a href=\"{url}\">{title}</a>\n",)
-    } else if action == "update" {
-        format!("<b>{user_name}</b> updated issue <a href=\"{url}\">{title}</a>\n",)
-    } else if action == "close" {
-        format!("<b>{user_name}</b> closed issue <a href=\"{url}\">{title}</a>\n",)
-    } else if action == "reopen" {
-        format!("<b>{user_name}</b> reopened issue <a href=\"{url}\">{title}</a>\n",)
-    } else {
-        format!("<b>{user_name}</b> {action} issue <a href=\"{url}\">{title}</a>\n",)
+    let action = match action.as_str() {
+        "open" => IssueAction::Open,
+        "update" => IssueAction::Update,
+        "close" => IssueAction::Close,
+        "reopen" => IssueAction::Reopen,
+        other => IssueAction::Other(other.to_string()),
+    };
+
+    match action {
+        IssueAction::Open => format!(
+            "<b>{user_name}</b> opened a new issue <a href=\"{url}\">{title}</a>\n{description}\n",
+        ),
+        IssueAction::Update => format!(
+            "<b>{user_name}</b> updated issue <a href=\"{url}\">{title}</a>\n{description}\n",
+        ),
+        IssueAction::Close => {
+            format!("<b>{user_name}</b> closed issue <a href=\"{url}\">{title}</a>\n",)
+        }
+        IssueAction::Reopen => {
+            format!("<b>{user_name}</b> reopened issue <a href=\"{url}\">{title}</a>\n",)
+        }
+        IssueAction::Other(action) => {
+            format!("<b>{user_name}</b> {action} issue <a href=\"{url}\">{title}</a>\n",)
+        }
     }
 }
